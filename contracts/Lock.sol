034b16e62a73cee306443c8a8e22944a0262c2c2
Hitesh Joshi <joshih738@gmail.com>
2023-04-25 14:06:59 +0530

    Initial commit

---
+++ contracts/Lock.sol
@@ -0,0 +1,33 @@
+// SPDX-License-identifier: GPL-3.0
+
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract shop
+{
+    struct Memo
+    {
+        string name;
+        string message;
+        uint timestamp;
+        address from;
+    }
+
+    Memo[] memos;
+    address payable owner;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    function buy(string memory name, string memory message) public payable
+    {
+        require(msg.value>0 , "Please enter Valid Amount ");
+        owner.transfer(msg.value);
+        memos.push(Memo(name,message,block.timestamp,msg.sender));
+    }
+
+    function getMemos() public view returns(Memo[] memory)
+    {
+        return memos;
+    }
+}
