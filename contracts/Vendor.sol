925399ebc75c20ae0cd1c46508aed63d2ab6c5b9
Hitesh Joshi <joshih738@gmail.com>
2023-04-26 12:28:13 +0530

    Deploying the contract using deploy.js

--- contracts/Vendor.sol
+++ contracts/Vendor.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.5.0 < 0.9.0;
 
-contract shop
+contract vendor
 {
     struct Memo
     {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,22 +6,59 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+
+async function getBalances(address)
+{
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function consoleBalances(addresses)
+{
+  let counter= 0;
+  for (const address of addresses )
+  {
+    console.log(`Address ${counter} balance:` , await getBalances(address));
+    counter++;
+  }      
+}
+
+async function consoleMemos(memos)
+{
+  for(const memo of memos){
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.from;
+    const message = memo.message;
+
+  console.log(`At ${timestamp}, name : ${name}, address : ${from} , message : ${message}`);
+
+  }
+}
+
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const [owner,from1,from2,from3] = await hre.ethers.getSigners();
+    const vendor = await hre.ethers.getContractFactory("vendor");
+    const contract = await vendor.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+    await contract.deployed();
+    console.log("Address of contracts:" , contract.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const addresses = [owner.address, from1.address, from2.address ,from3.address];
+    console.log("Before buying");
+    await consoleBalances(addresses);
 
-  await lock.deployed();
+    const amount = { value:hre.ethers.utils.parseEther("1")};
+    await contract.connect(from1).buy("from1", "very nice service",amount);
+    await contract.connect(from2).buy("from2", "good service",amount);
+    await contract.connect(from3).buy("from3", "overrated",amount);
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    console.log("After buying");
+    await consoleBalances(addresses);
+
+    const memos = await contract.getMemos();
+    consoleMemos(memos);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
