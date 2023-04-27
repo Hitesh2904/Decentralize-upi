eae2e7042142e386eac7af89a2ed4ca66e508d5a
Hitesh Joshi <joshih738@gmail.com>
2023-04-27 16:11:47 +0530

    D app interacting with contract

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import abi from "./contract/vendor.json"
+import {useState,useEffect} from 'react';
+import {ethers} from "ethers";
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [state,setState] = useState({
+    provider:null, 
+    signer:null,
+    contract:null
+  })
+  
+  useEffect(()=> {
+    const connectWallet = async () => {
+      const contractAddress = "0xfb407e71a906c1891584cb05187d27aa049a1a79";
+      const contractABI = abi.abi;
+      
+      try{
+        const {ethereum} = window;
+
+        if(ethereum)
+        {
+          const account = await ethereum.request({method : "eth_requestAccounts",})
+        }
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress,contractABI,signer);
+        setState({provider,signer,contract});
+      }catch(error)
+      {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  },[]);
+  console.log(state);
+
+  return (<div className="App"></div>);
 }
 
 export default App;
